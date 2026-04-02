c18acd82f6e0694941ac464652465c2496d202f0
iliyasis <webiliya1504@gmail.com>
2026-04-03 00:18:30 +0330

    some change

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from blog_app.models import Post ,Category
+from django.core.paginator import Paginator
 
 # Create your views here.
 
+
+page_names = {
+    "home_page": "خانه",
+    "post_list": "بلاگ ها",
+    "category_detail": "دسته بندی ها"
+}
+
 def post_list(request):
-    active_head = "post_list"
-    posts = Post.objects.all()
-    return render(request,"blog_app/post_list.html", {"posts":posts, "active_head":active_head})
+    paginator = Paginator(Post.objects.all(), 3)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+    page_name = page_names[request.resolver_match.url_name]
+    return render(request,"blog_app/post_list.html", {"posts":posts, "page_name":page_name})
 
 def category_detail(request, pk):
-    print(pk)
     category = get_object_or_404(Category, pk=pk)
-    posts = category.posts.all()
-    return render(request, "blog_app/post_list.html", {"posts":posts})
+    paginator = Paginator(category.posts.all(), 3)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+    page_name = page_names[request.resolver_match.url_name]
+    return render(request, "blog_app/post_list.html", {"posts":posts, "page_name":page_name, "page_name2":category.title})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
